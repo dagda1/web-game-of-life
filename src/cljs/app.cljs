cc9dc2ae62076229e6b995c0c2396578567c05a4
dagda1 <dagda1@scotalt.net>
2014-05-11 20:40:59 +0100

    render rows and cells

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -4,21 +4,24 @@
 
 (enable-console-print!)
 
-;; (def app-state (atom [[1 2 1] [2 1 1] [1 2 1]]))
-
-(def app-state (atom {:world [1 2 1]}))
+(def app-state (atom {:world [[1 2 1] [2 1 1] [1 2 1]]}))
 
 (defn cell
   [text]
   (om/component
     (dom/td nil text)))
 
+(defn row
+  [data]
+    (om/component
+      (apply dom/tr nil
+        (om/build-all cell data))))
+
 (defn world-view
   [data owner]
     (om/component
-      (dom/table nil
-        (apply dom/tr nil
-          (om/build-all cell (:world data))))))
+      (apply dom/table nil
+        (om/build-all row (:world data)))))
 
 (om/root
   world-view
