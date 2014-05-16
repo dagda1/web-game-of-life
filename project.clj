798a1e5e33b38138f1edb2a296e7c6f669254ee9
dagda1 <dagda1@scotalt.net>
2014-05-16 22:14:37 +0100

    add midje

--- project.clj
+++ project.clj
@@ -9,21 +9,37 @@
     [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
 
     [ring/ring-core "1.2.0"]
-    [ring/ring-jetty-adapter "1.1.0"]
+    [ring/ring-jetty-adapter "1.2.0"] ; Web Server https://github.com/ring-clojure/ring
 
+    [cheshire "5.3.1"] ; JSON de/encoding https://github.com/dakrone/cheshire
     [liberator "0.11.0"]
 
-    [compojure "1.1.6"]
-    [liberator "0.11.0"]
+    [compojure "1.1.8"] ; Web routing https://github.com/weavejester/compojure
+    [liberator "0.11.0"] ; WebMachine (REST state machine) port to Clojure https://github.com/clojure-liberator/liberator
 
     [om "0.5.0"]
-    [secretary "1.1.0"]
   ]
+
+  :profiles {
+    :test {
+      :dependencies [
+                     [midje "1.6.3"] ; Example-based testing https://github.com/marick/Midje
+                    ]
+      :plugins [
+                [lein-midje "3.1.3"] ; Example-based testing https://github.com/marick/lein-midje
+                ]
+    }
+  }
+
+  :aliases {
+    "midje" ["with-profile" "test" "midje"] ; run unit tests
+  }
 
   :plugins
   [
     [lein-cljsbuild "1.0.2"]
     [lein-ring "0.8.10"]
+    [lein-midje "3.1.3"] ; Example-based testing https://github.com/marick/lein-midje
   ]
 
   :source-paths ["src/clj"]
